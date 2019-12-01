03f92cd7239f3a794d87724b884d7c6e32b898ce
TanChao <chao.tan@jishi.ca>
2019-12-01 16:29:12 +0800

    disable https

--- src/ios/AppDelegate+IonicDeeplink.m
+++ src/ios/AppDelegate+IonicDeeplink.m
@@ -16,12 +16,12 @@ static NSString *const PLUGIN_NAME = @"IonicDeeplinkPlugin";
 
 @end
 
-@implementation NSURLRequest(DataController) 
-+ (BOOL)allowsAnyHTTPSCertificateForHost:(NSString *)host 
-{ 
-return YES; 
-} 
-@end
+// @implementation NSURLRequest(DataController) 
+// + (BOOL)allowsAnyHTTPSCertificateForHost:(NSString *)host 
+// { 
+// return YES; 
+// } 
+// @end
 @implementation AppDelegate (IonicDeeplinkPlugin)
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
